bee78caae1dcf5a15624a0f2bbb0175f736389eb
lnoppinger <lukas.noppinger@gmail.com>
2024-03-29 02:50:32 +0100

    built android app

---
+++ android-app/app/build/generated/source/buildConfig/release/simple_ttt/lnoppinger/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package simple_ttt.lnoppinger;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "simple_ttt.lnoppinger";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1";
+}
---
+++ android-app/app/src/main/java/simple_ttt/lnoppinger/DelegationService.java
@@ -0,0 +1,14 @@
+package simple_ttt.lnoppinger;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
